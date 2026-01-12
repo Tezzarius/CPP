018985eaa51cacbd5eff1683f2a1a8ce9a0a9137
bschwarz <bschwarz@student.42vienna.com>
2026-01-12 13:00:34 +0100

    at work

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -1,8 +1,23 @@
 #include "ClapTrap.hpp"
 
 int main() {
-	ClapTrap test("anyone");
+	ClapTrap a("Alpha");
+	ClapTrap b("Beta");
 
-	test.attack("everyone");
+	a.attack(b.getName());
+	a.takeDamage(3);
+	a.beRepaired(2);
+
+	for (int i = 0; i < 11; i++)
+		a.attack("Dummy");
+
+	a.beRepaired(5);
+
+	b.takeDamage(100);
+	std::cout << b.getName() << " has " << b.getHitPoints() << " hitpoints left" << std::endl;
+
+	b.attack("Nobody");
+	b.beRepaired(10);
+
 	return 0;
 }
