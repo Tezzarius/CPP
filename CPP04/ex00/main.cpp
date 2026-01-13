160bc7b3c4f3e6315232b53d1985edc825896fc1
bschwarz <bschwarz@student.42vienna.com>
2026-01-13 18:18:25 +0100

    starting CPP04

---
+++ CPP04/ex00/main.cpp
@@ -0,0 +1,31 @@
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongCat.hpp"
+
+int main() {
+	const Animal* alpha = new Animal();
+	const Animal* dog = new Dog();
+	const Animal* cat = new Cat();
+
+	std::cout << "Dog type: " << dog->getType() << std::endl;
+	std::cout << "Cat type: " << cat->getType() << std::endl;
+	std::cout << "The Cat make: ";
+	cat->makeSound();
+	std::cout << "The Dog make: ";
+	dog->makeSound();
+	std::cout << "The Animal make: ";
+	alpha->makeSound();
+
+	std::cout << std::endl;
+
+	const WrongAnimal* beta = new WrongAnimal();
+	const WrongAnimal* wrongCat = new WrongCat();
+
+	std::cout << "WrongCat type: " << wrongCat->getType() << std::endl;
+	std::cout << "The WrongCat make: ";
+	wrongCat->makeSound();
+	std::cout << "The WrongAnimal make: ";
+	beta->makeSound();
+	
+	return 0;
+}
