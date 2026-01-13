21218cff97cde15d2cb8b52cf55685a6f1ee594b
bschwarz <bschwarz@student.42vienna.com>
2026-01-13 18:22:49 +0100

    starting CPP04

---
+++ CPP04/ex01/Cat.cpp
@@ -0,0 +1,24 @@
+#include "Cat.hpp"
+
+Cat::Cat() {
+	type = "Cat";
+	std::cout << "Cat constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat &other) : Animal(other) {
+	std::cout << "Cat was copied" << std::endl;
+}
+
+Cat &Cat::operator=(const Cat &other) {
+	Animal::operator=(other);
+	std::cout << "Cat was assigned" << std::endl;
+	return *this;
+}
+
+Cat::~Cat() {
+	std::cout << "Cat is destroyed" <<std::endl;
+}
+
+void Cat::makeSound() const {
+	std::cout << "* Meow! Meow! *" << std::endl;
+}
