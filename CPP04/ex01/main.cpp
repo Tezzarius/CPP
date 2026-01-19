a2412f5a3eef79e8ee13353f4e6644a3c0cf9567
Tezzarius <mortis@live.at>
2026-01-19 15:47:31 +0100

    at work

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -3,35 +3,45 @@
 #include "WrongCat.hpp"
 
 int main() {
-	const Animal* alpha = new Animal();
 	const Animal* dog = new Dog();
 	const Animal* cat = new Cat();
 
 	std::cout << "Dog type: " << dog->getType() << std::endl;
 	std::cout << "Cat type: " << cat->getType() << std::endl;
+
 	std::cout << "The Cat make: ";
 	cat->makeSound();
 	std::cout << "The Dog make: ";
 	dog->makeSound();
-	std::cout << "The Animal make: ";
-	alpha->makeSound();
-	delete alpha;
+
 	delete dog;
 	delete cat;
 
 	std::cout << std::endl;
 
-	const WrongAnimal* beta = new WrongAnimal();
-	const WrongAnimal* wrongCat = new WrongCat();
+	Animal *animals[10];
 
-	std::cout << "WrongCat type: " << wrongCat->getType() << std::endl;
-	std::cout << "The WrongCat make: ";
-	wrongCat->makeSound();
-	std::cout << "The WrongAnimal make: ";
-	beta->makeSound();
-	
-	delete beta;
-	delete wrongCat;
+	for (int i = 0; i < 5; i++)
+		animals[i] = new Dog();
+	for (int i = 5; i < 10; i++)
+		animals[i] = new Cat();
+
+	for (int i = 0; i < 10; i++)
+		delete animals[i];
+
+	std::cout << std::endl;
+
+	Dog a;
+	Dog b(a);
+
+	std::cout << std::endl;
+
+	Dog c;
+	Dog d;
+
+	d = a;
+
+	std::cout << std::endl;
 
 	return 0;
 }
