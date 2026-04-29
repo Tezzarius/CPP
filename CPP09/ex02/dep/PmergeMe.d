a03d3d2cbcfde9911a279c3583971aca77fb1775
Benjamin Schwarz <bschwarz@c3r8p8.42vienna.com>
2026-04-29 14:56:17 +0200

    fin

--- CPP09/ex02/dep/PmergeMe.d
+++
@@ -1,8 +0,0 @@
-obj/PmergeMe.o: src/PmergeMe.cpp include/PmergeMe.hpp \
-  include/PmergeMe.tpp include/explain.tpp
-
-include/PmergeMe.hpp:
-
-include/PmergeMe.tpp:
-
-include/explain.tpp:
--- CPP09/ex02/dep/main.d
+++
@@ -1,8 +0,0 @@
-obj/main.o: src/main.cpp include/PmergeMe.hpp include/PmergeMe.tpp \
-  include/explain.tpp
-
-include/PmergeMe.hpp:
-
-include/PmergeMe.tpp:
-
-include/explain.tpp:
